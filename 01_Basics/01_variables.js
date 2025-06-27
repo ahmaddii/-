f06a76a299fa24cd68c474cb1a417fb80c605f87
ahmaddii <malikahmad6666666@gmail.com>
2025-06-27 20:58:43 +0500

    Initial commit

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = "2380230";
+let accountName = "Malik Ahmad Rasheed";
+var accountCity = "Mianwali";
+accountDetails = "Savings";
+
+// javascript let or const are blocked scoped while var are funcional scope 
+// we not prefer to use var
+
+
+console.table([accountId,accountName,accountCity,accountDetails]);
