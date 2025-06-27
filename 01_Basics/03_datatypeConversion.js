f8a0c48d96e3ec827d13d5a9a7339ff2c2aca6fc
ahmaddii <malikahmad6666666@gmail.com>
2025-06-28 00:59:33 +0500

    data type conversion

---
+++ 01_Basics/03_datatypeConversion.js
@@ -0,0 +1,26 @@
+//let Score = undefined;
+
+//console.log(typeof (Score));
+
+//let valueInNumber = Number(Score);
+//console.log(valueInNumber);
+
+
+// '33' => 33 Number hojata
+// "33abc" => NAN
+// "true" => 1 , false => 0
+
+let isLoggedin = "Ahmad";
+
+let BooleanIsLoggedIn = Boolean(isLoggedin);
+console.log(BooleanIsLoggedIn);
+
+// 1 => it converts to true 
+// 0 => false
+// "Ahmad" => true
+// " " => false
+
+let sumNumber = 33;
+
+let StringNumber = String(sumNumber);
+console.log(typeof StringNumber);
