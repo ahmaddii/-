90b0b62b6aae6a5ab90eaa503bee87f138424e04
ahmaddii <malikahmad6666666@gmail.com>
2025-06-28 00:31:41 +0500

    dataTypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"
+const { type } = require("express/lib/response");
+
+ // this will include all latest operations of js
+
+//alert(3 + 3); // we are using node js but in browser engine it can execute
+
+console.log(3+3);
+
+let id = 123; // int
+let name = "Ahmad Rasheed"; // string
+let isLogged = true; // boolean
+let state; // undefined
+
+// Data Types
+// 1. Number 2 => power 53
+// 2. bigInt
+// 3. String => ""
+// 4. boolean => true/false
+// 5. Null => Stand Alone Value type is object e.g if we want temp from server and it shows some error so we want null value to show not zero as zero is the temperature
+// 6. Undefined =>
+// 7. Symbol = Uniquness
+
+console.log(typeof null);
+
+console.log(typeof undefined);
