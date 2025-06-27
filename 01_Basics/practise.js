e50133780e7ea16209aaa1de75392ba5e27d6218
ahmaddii <malikahmad6666666@gmail.com>
2025-06-28 01:07:49 +0500

    practise

---
+++ 01_Basics/practise.js
@@ -0,0 +1,17 @@
+const { log } = require("electron-builder");
+
+let Score = null;
+console.log(typeof Score);
+
+let convertToString = String(Score);
+console.log(typeof convertToString);
+console.log(convertToString);
+
+let Name = "Ahmad";
+console.log(typeof Name);
+
+let conertName = Number(Name);
+console.log(typeof conertName);
+console.log(conertName);
+
+
