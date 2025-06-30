fbab24d3f9027fd7a84d19bb5e186ffb2cd49582
ahmaddii <malikahmad6666666@gmail.com>
2025-06-30 23:41:26 +0500

    maths and numbers

---
+++ 01_Basics/maths.js
@@ -0,0 +1,41 @@
+const score = 400; // simple define
+
+console.log(400);
+
+
+const balance = new Number(100); // explicit define
+
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2)); // to display how much decimal number
+
+const otherNumber = 23.75543;
+
+console.log(otherNumber.toPrecision(2)); // it basically round after decimal up to 2 preciosion
+
+
+//console.log(typeof balance);
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString('en-In'));
+
+// _______ Maths _______
+
+console.log(Math);
+
+console.log(Math.abs(-4)); // absoulte math function convert negative number into positive
+
+console.log(Math.round(4.6));
+console.log(Math.ceil(4,2)); // uper wali value le ga
+
+console.log(Math.floor(4,7));
+
+console.log(Math.sqrt(5));
+
+console.log(Math.min(4,322,2,1)); // find min value
+
+console.log(Math.max(22,34,12)); // find max value 
+
+
+
