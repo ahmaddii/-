99378a159f6aa6bedf1d4253e9a97f977da9fc26
ahmaddii <malikahmad6666666@gmail.com>
2025-07-01 20:21:40 +0500

    practise arrays

---
+++ 02_Basics/arrays2.js
@@ -0,0 +1,26 @@
+//const array = [];
+
+//array.push("Ahmad","Ali","Fahad");
+//array.pop();
+
+//console.log(array.length);
+
+//console.log(array);
+
+const myArray = [2,3,4,5,6,7];
+
+// slice and splice
+
+console.log("A",myArray);
+
+const myn1 = myArray.slice(3,5);
+console.log(myn1);
+console.log("B",myArray);
+
+const myn2 = myArray.splice(2,4);
+console.log("C",myArray);
+console.log(myn2);
+
+
+
+
